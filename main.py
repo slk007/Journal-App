c26b03a4930ed6751f1e6a4e0e6bc70ae79cd1da
Shubham Soni <shubham.soni.lko@gmail.com>
2020-02-02 18:27:17 +0530

    made printing of only 10 logs possible

--- main.py
+++ main.py
@@ -3,24 +3,30 @@ from users import user
 
 
 def new_log(username):
-    with open('log.txt', 'a') as writer:  # file object for writing
+    with open('log.txt', 'r+') as reader:  # file object for reading and writing
         print("Enter New Journal")
+        content = reader.read()     # getting file content into a single object
         journal = input()  # journal string is being entered  by the user
         L = Log(journal)  # making an object of Log class from data.py file
         j = L.get_message()  # getting the journal with timestamp
-        writer.seek(0)
-        writer.write(username + "$" + j + "\n")  # writing the journal to file
+        reader.seek(0, 0)
+        reader.write(username + "$" + j + "\n" + content)  # writing the journal to file
         print("New Log Successfully Written")
 
 
 def view_log(username):
     try:  # handling the exception if no journal was created yet
         # Hence no file was created successfully
+        no_of_journals = 0
         with open('log.txt', 'r') as reader:  # file object for reading
             for i in reader:  # printing the journals
                 s_part = list(i.split("$"))
                 if s_part[0] == username:
                     print(s_part[1])
+                    no_of_journals += 1
+                if no_of_journals == 10:
+                    break
+
     except FileNotFoundError or IndexError:
         print("No Journals Yet. Press Y/n to Create New")
         want = input()
